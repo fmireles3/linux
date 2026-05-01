4c7745ded36172fe369cda4e761eca51018cb919
fmireles3 <133544667+fmireles3@users.noreply.github.com>
2026-05-01 06:54:30 -0500

    Add files via upload

--- fish/config.fish
+++ fish/config.fish
@@ -1,12 +1,12 @@
-if status is-interactive
+#if status is-interactive
 # Commands to run in interactive sessions can go here
-end
+#end
 
 #Default terminal
 export TERMINAL=/usr/bin/footclient
 
 #Launch on startup of terminal
-#fastfetch
+#fastfetch --logo-padding-top 3 --logo-padding-left 3
 
 #Alias
 alias cat="bat"
@@ -14,6 +14,7 @@ alias mi="sudo micro"
 alias ff="fastfetch"
 alias pac="sudo pacman -Sy"
 alias pacr="sudo pacman -Rs"
+#alias cd="zoxide"
 
 if status --is-interactive
     if not set -q DISPLAY; and not set -q WAYLAND_DISPLAY 
@@ -26,3 +27,11 @@ end
 # Created by `pipx` on 2026-01-12 14:33:05
 set PATH $PATH /home/fmireles3/.local/bin
 
+fish_config theme choose default-rgb
+#set fish_color_command brcyan 
+#set fish_color_error brred
+#set fish_color_param brblue
+#set fish_color_comment brpurple
+#set fish_color_cwd brgreen
+#set fish_color_match brmagenta
+#set fish_color_quote brmagenta
